2824eb0e55fa71dc9f62982be35243d266f665e5
Khuong Mai <maingockhuong89@gmail.com>
2023-03-01 14:22:28 -0600

    .

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,7 +1,6 @@
 class Search < ApplicationRecord
   def search_books
     books = Book.all
-
     books = books.where('title LIKE ? or isbn13 LIKE ?', "%#{keywords}%") if keywords.present?
     return books
   end
